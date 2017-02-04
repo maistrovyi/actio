d44274fc18b01fbf95d63026e13b6d84bb9f91a6
Vladimir Maystrovoy <maystrovyy@gmail.com>
2017-02-04 18:23:17 +0200

    Init

---
+++ src/main/java/com/actio/ActioApplication.java
@@ -0,0 +1,12 @@
+package com.actio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActioApplication.class, args);
+	}
+}
---
+++ src/test/java/com/actio/ActioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.actio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
